1ff1fdc299c4d655fffe5c9358540b9ae18a9674
Amethyst <oconnellamethyst@gmail.com>
2019-04-30 10:44:45 -0500

    Combiner code

--- woole022_Lab6.X/flowControl.c
+++ woole022_Lab6.X/flowControl.c
@@ -1,18 +1,17 @@
 /*
  * File:   flowControl.c
- * 
+ *
  * Usage:  This library controls Adafruit's Latching Relay, product #2923.
  *   RB0 is used to 'SET' the relay (COM connected to N.O.),
  *   RB1 is used to 'RESET' the relay (COM connected to N.C.).
  *   Timer 1 is used for the pause-for-n-seconds function.
- * 
+ *
  * Author: Scott Deyo
  *
  * Created on 4/26/19
  */
 
 #include "xc.h"
-#include "finProj.h"
 
 void flowControlInit(void)
 {
@@ -21,31 +20,31 @@ void flowControlInit(void)
     LATB &= 0x3FFF;         // make sure they start low
 }
 
-//void pause(uint8_t pause_in_seconds)
-//{
-//    int i = 0;
-//    while(i < pause_in_seconds)
-//    {
-//        while(!_T1IF);
-//        _T1IF = 0;
-//        i++;
-//    }
-//}
+void pause(uint8_t pause_in_seconds)
+{
+    int i = 0;
+    while(i < pause_in_seconds)
+    {
+        while(!_T1IF);
+        _T1IF = 0;
+        i++;
+    }
+}
 
 void waterTime(uint8_t water_time_in_seconds)
 {
     uint32_t i;
-    
+
     // Latch 'SET' on relay to turn flow solenoid ON
     LATB = 0x8000;         // RB0 latches solenoid ON
     for(i = 0; i < 160000; i++)     // keep high >10ms to latch
     {
         asm("nop");
-    }             
+    }
     LATB = 0;         // Turn RB0 OFF to conserve power; solenoid remains latched ON
-    
+
     delay(water_time_in_seconds);   // Water keeps flowing
-    
+
     // Latch 'UNSET' on relay to turn flow solenoid OFF
     LATB = 0x4000;         // RB1 latches solenoid OFF
     for(i = 0; i < 160000; i++)     // keep high >10ms to latch
@@ -54,4 +53,3 @@ void waterTime(uint8_t water_time_in_seconds)
     }
     LATB = 0;         // ... RB1 is turned off to conserve power; solenoid remains latched OFF
 }
-
--- woole022_Lab6.X/woole022_Lab6.c
+++ woole022_Lab6.X/woole022_Lab6.c
@@ -2,6 +2,7 @@
 #include <stdio.h> //for sprintf
 #include "Display6.h"
 #include "woole022_Lab6.h"
+#include "flowControl.h"
 
 #pragma config ICS = PGx1          // Comm Channel Select (Emulator EMUC1/EMUD1 pins are shared with PGC1/PGD1)
 #pragma config FWDTEN = OFF        // Watchdog Timer Enable (Watchdog Timer is disabled)
@@ -11,14 +12,14 @@
 #pragma config I2C1SEL = PRI       // I2C1 Pin Location Select (Use default SCL1/SDA1 pins)
 #pragma config IOL1WAY = OFF       // IOLOCK Protection (IOLOCK may be changed via unlocking seq)
 #pragma config OSCIOFNC = ON       // Primary Oscillator I/O Function (CLKO/RC15 functions as I/O pin)
-#pragma config FCKSM = CSECME      // Clock Switching and Monitor (Clock switching is enabled, 
+#pragma config FCKSM = CSECME      // Clock Switching and Monitor (Clock switching is enabled,
 #pragma config FNOSC = FRCPLL      // Oscillator Select (Fast RC Oscillator with PLL module (FRCPLL))
 
 
 int setup(void){
     CLKDIVbits.RCDIV = 0;
     AD1PCFG = 0xFFFE; //everything digital except AN0
-    
+
     //LCD stuff
     TRISA = 0b1;  //set RA0 for output to LED
     TRISB = 0;  //set RB8 to output
@@ -27,7 +28,7 @@ int setup(void){
     I2C2BRG = 157; //Baud rate generator
     _MI2C2IF = 0;
     I2C2CONbits.I2CEN = 1;
-    
+
     //Analog Stuff
     initBuffer();   //Sets all values to 0
     AD1CON1bits.SSRC = 0b010; //sets to timer 3
@@ -36,18 +37,26 @@ int setup(void){
     AD1CON3 = 0x0100;
     AD1CON3bits.ADCS = 1;
     IFS0bits.AD1IF = 0;
-    IEC0bits.AD1IE = 1;  
-    PORTB = 0xffff;     
-    
-    //Timer 3 stuff 
+    IEC0bits.AD1IE = 1;
+    PORTB = 0xffff;
+
+    // TMR1 used for simple delay
+    T1CON = 0;
+    PR1 = 62500;
+    TMR1 = 0;
+    T1CONbits.TCKPS = 3;
+    T1CONbits.TON = 1;
+
+    //Timer 3 stuff
     T3CONbits.TCKPS0 = 0; //1:64
     T3CONbits.TCKPS1 = 1;
     TMR3 = 0;
     PR3 = 15625; //Once ever 1/16th of a second.
     T3CONbits.TON = 1;
-   
+
     AD1CON1bits.ADON = 1;
     lcd_init();
+    flowControlInit();
     return 0;
 }
 
@@ -58,18 +67,18 @@ void __attribute__((interrupt, auto_psv)) _ADC1Interrupt(){
     IFS0bits.AD1IF = 0; //read voltage from A0 for 16 times a second
     adValue = ADC1BUF0;
 }
-    
+
 int main(void){
     setup();
     float curVal;
-    
+
     while(1){
         lcd_setCursor(0,0);
         curVal = (3.3/1024)*adValue*100/2.6;
         sprintf(adStr, "%6.2f %%", curVal);
         lcd_printStr(adStr);
-          
-        lcd_setCursor(0,1);        
+
+        lcd_setCursor(0,1);
         if(curVal >= 80)      lcd_printStr("Too wet");
         else if(curVal >= 60) lcd_printStr("Bit wet");
         else if(curVal >= 40) lcd_printStr("Normal");
@@ -79,6 +88,6 @@ int main(void){
         putVal(getAvg());
         delay(100);
     }
-    
+
     return 0;
 }
